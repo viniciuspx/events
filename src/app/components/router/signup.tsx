d90604d3b2df29c38a3551b00c85d4cbfd579709
viniciuspx <vinicius.gabriel.px@gmail.com>
2024-02-19 14:59:19 -0300

    fix email to lowercase in signup

--- src/app/components/router/signup.tsx
+++ src/app/components/router/signup.tsx
@@ -8,7 +8,7 @@ export const signup = async (event: any) => {
       .post(process.env.API_URL + "/auth/register", {
         name: event.target.elements.name.value,
         username: event.target.elements.username.value,
-        email: event.target.elements.email.value,
+        email: event.target.elements.email.value.toLowerCase(),
         password: event.target.elements.password.value,
       })
       .then((response) => {
